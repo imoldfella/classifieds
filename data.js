b73654b46486cb1c682df80a3c025c70127ccbbf
ger <68071939+gerzarko@users.noreply.github.com>
2023-03-17 14:30:14 -0400

    Add responsive Navbar

---
+++ data.js
@@ -0,0 +1,26 @@
+// import { getPermalink, getBlogPermalink, getAsset } from "./utils/permalinks";
+
+export const headerData = {
+  links: [
+    {
+      text: "Home",
+      link: "/",
+    },
+    {
+      text: "Join",
+      link: "/join",
+    },
+    {
+      text: "Login",
+      link: "/login",
+    },
+    {
+      text: "Contact Us",
+      link: "/contact-us",
+    },
+    {
+      text: "Donate",
+      link: "/donate",
+    },
+  ],
+};
